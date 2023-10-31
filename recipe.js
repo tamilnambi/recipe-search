9ac024fb0746da25387a658ad3bdcd2f8e627f2e
tamilnambi <tamilnambicse@gmail.com>
2023-10-31 13:58:57 +0530

    completed

--- recipe.js
+++ recipe.js
@@ -2,6 +2,7 @@ const uri = sessionStorage.getItem("id");
 const app_id = sessionStorage.getItem("app_id");
 const app_key = sessionStorage.getItem("app_key");
 const endpoint = sessionStorage.getItem('endpoint');
+var healthLabelButtons = '';
 
 // Create the URL with query parameters
 const url = new URL(endpoint);
@@ -26,6 +27,12 @@ fetch(url)
   document.getElementById('recipeName').innerHTML = data[0].label;
   document.getElementById('recipeImage').src = data[0].image;
 
+  data[0].healthLabels.forEach( item =>{
+
+        healthLabelButtons += `<button type="button" class="btn btn-warning m-2">${item}</button>`;
+  });
+  document.getElementById('healthLabels').innerHTML = healthLabelButtons;
+
   const ulContainer = document.getElementById('ingredients');
   const ulElement = document.createElement("ul");
   ulElement.className = "list-group";
@@ -37,8 +44,9 @@ fetch(url)
     liElement.textContent = item;
     ulElement.appendChild(liElement);
   });
-
   document.getElementById('instructions').src = data[0].url;
+
+
 })
 .catch((error) => {
   console.error("Error:", error);
