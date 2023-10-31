2cdea17a8ecfa70b51e12564f9cbcf4187fb4745
tamilnambi <tamilnambicse@gmail.com>
2023-10-31 11:55:35 +0530

    Add files via upload

--- recipe.js
+++ recipe.js
@@ -2,16 +2,13 @@ const uri = sessionStorage.getItem("id");
 const app_id = sessionStorage.getItem("app_id");
 const app_key = sessionStorage.getItem("app_key");
 const endpoint = sessionStorage.getItem('endpoint');
-console.log(uri);
-console.log(app_id);
-console.log(app_key);
-console.log(endpoint);
+
 // Create the URL with query parameters
 const url = new URL(endpoint);
 url.searchParams.append("r", uri);
 url.searchParams.append("app_id", app_id);
 url.searchParams.append("app_key", app_key);
-console.log(url);
+
 // Make the API request using the fetch function
 fetch(url)
 .then((response) => {
@@ -25,6 +22,23 @@ fetch(url)
 .then((data) => {
   // Process the recipe data (it may contain multiple recipes)
   console.log(data);
+
+  document.getElementById('recipeName').innerHTML = data[0].label;
+  document.getElementById('recipeImage').src = data[0].image;
+
+  const ulContainer = document.getElementById('ingredients');
+  const ulElement = document.createElement("ul");
+  ulElement.className = "list-group";
+  ulContainer.appendChild(ulElement);
+  
+  data[0].ingredientLines.forEach( item => {
+    const liElement = document.createElement('li');
+    liElement.className = "list-group-item";
+    liElement.textContent = item;
+    ulElement.appendChild(liElement);
+  });
+
+  document.getElementById('instructions').src = data[0].url;
 })
 .catch((error) => {
   console.error("Error:", error);
